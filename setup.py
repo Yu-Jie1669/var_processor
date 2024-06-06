b7bb7229f892f0251af1d7397a48fa6075e0e686
御姐 <162095214@qq.com>
2024-06-06 22:25:52 +0800

    common modify

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="var_process",
+    name="var_procesor",
     version="0.0.1",
     author="Yj Lin",
     author_email="yjlin2001@gmail.com",
-    description="Auto process vars in your pipeline.",
+    description="Var_processor is a Python library that can automatically handle common variable operations in projects.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Yu-Jie1669/var_process",
+    url="https://github.com/Yu-Jie1669/var_procesor",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
